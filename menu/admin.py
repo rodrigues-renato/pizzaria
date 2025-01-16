0df84a0995e494e2893c44a1cadb6e9693610724
Renato Rodrigues <renato.rodrigues.si@hotmail.com>
2025-01-16 11:45:15 -0300

    menu - ajuste na visualização na aba do admin

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from menu.models import Cardapio
 
 # Register your models here.
+
+@admin.register(Cardapio)
+class CardapioAdmin(admin.ModelAdmin):
+    list_display = 'id', 'preco'
+    ordering = '-id',
