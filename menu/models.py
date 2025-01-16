b729dce00b00f6ecd3e91d629a3d0d8f0cc77613
Renato Rodrigues <renato.rodrigues.si@hotmail.com>
2025-01-16 11:44:31 -0300

    criação models do cardápio

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Cardapio(models.Model):
+    class Meta:
+        verbose_name = 'Cardápio'
+        verbose_name_plural = 'Cardápios'
+    
+    nome = models.CharField(max_length=30)
+    preco = models.FloatField()
+    ingrediente = models.TextField()
+    categoria = models.CharField(max_length=50, choices=[("Pizza", "Pizza"), ("Bebida", "Bebida")])
